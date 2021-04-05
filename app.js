657b171cf70c4289841ed04d98acc929ac7aba65
unknown <aelise17264@gmail.com>
2021-04-05 17:59:07 -0600

    form added, inline styles added

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     var today = new Date()
-    var thisDay = today.toString().split(' ')[0] + "day"
-
+    // var thisDay = today.toString().split(' ')[0] + "day"
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var thisDay = today.toLocaleDateString("en-US", options)
     if(today.getDay() === 6 || today.getDay() === 0){
         res.send("<h1>Everybody's working for the weekend</h1>")
     }else{
