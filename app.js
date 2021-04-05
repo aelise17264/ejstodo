ca9a19c06421e47ac9b41385575e34e2f7edbdd3
unknown <aelise17264@gmail.com>
2021-04-05 17:31:07 -0600

    added scriplet tags

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
     var today = new Date()
-    var thisDay = today.toString().split(' ')[0]
+    var thisDay = today.toString().split(' ')[0] + "day"
 
     if(today.getDay() === 6 || today.getDay() === 0){
         res.send("<h1>Everybody's working for the weekend</h1>")
