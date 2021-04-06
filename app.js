e8a3f32b110f68ad3079bbd155e69e10525aafbc
unknown <aelise17264@gmail.com>
2021-04-05 19:23:31 -0600

    added header & footer pages to keep styling the same across site, updated styles

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 
 var items = []
 
+let workItems = []
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
@@ -22,15 +24,36 @@ app.get("/", function(req, res){
     if(today.getDay() === 6 || today.getDay() === 0){
         res.send("<h1>Everybody's working for the weekend</h1>")
     }else{
-        res.render("list", {day: thisDay, newToDo: items})
+        res.render("list", {listTitle: thisDay, newToDo: items})
     }
 })
 
-app.post("/", function(req, res){
-    var item = req.body.newItem
-    items.push(item)
+// app.post("/", function(req, res){
+//     var item = req.body.newItem
+//     items.push(item)
 
-    res.redirect("/")
+//     res.redirect("/")
+// })
+
+app.post("/", function(req, res){
+    let item = req.body.newItem
+
+    if(req.body.list === "Work"){
+        workItems.push(item)
+        res.redirect("/work")
+    }else{
+        items.push(item)
+        res.redirect("/")
+    }
+
+})
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work", newToDo: workItems})
+})
+
+app.get("/about", function(req, res){
+    res.render("about")
 })
 
 app.listen(3000, function(){
