ab2df22fa3f95c13939ceccbe44e9a445ad81db9
unknown <aelise17264@gmail.com>
2021-04-12 15:11:22 -0600

    fixed bug that kept database from spinning up

--- app.js
+++ app.js
@@ -9,21 +9,49 @@ app.use(express.static("public"))
 
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhose:27017/todolistDB", {useNewUrlParser: true})
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true})
+
+const itemSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item"
+})
+
+const item3 = new Item({
+    name: "Hit the checkbox to delete your finished item"
+})
+
+const defaultItems = [item1, item2, item3]
+
+Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Success")
+    }
+})
 
 app.get("/", function(req, res){
-    var today = new Date()
+    // var today = new Date()
     // var thisDay = today.toString().split(' ')[0] + "day"
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    let thisDay = today.toLocaleDateString("en-US", options)
+    // var options = {
+    //     weekday: "long",
+    //     day: "numeric",
+    //     month: "long"
+    // }
+    // let thisDay = today.toLocaleDateString("en-US", options)
     // if(today.getDay() === 6 || today.getDay() === 0){
     //     res.send("<h1>Everybody's working for the weekend</h1>")
     // }else{
-        res.render("list", {listTitle: thisDay, newToDo: items})
+        // res.render("list", {listTitle: "Today", newToDo: Item})
     //}
 })
 
